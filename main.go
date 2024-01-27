ef4b0ddd0ec31235d269b19be08b939d2ef9427f
misaka10843 <misaka10843@outlook.jp>
2024-01-27 21:59:14 +0800

    将端口改为8051

--- main.go
+++ main.go
@@ -20,5 +20,5 @@ func main() {
 	// AS
 	router.AsRouter(r)
 
-	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8051") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
